e66598e0a09769b2e5ee2c5fa538049f19787840
suhailvs <suhailvs@gmail.com>
2021-03-11 09:27:01 +0530

    added question and questions designs

--- questions/static/js/questions.js
+++ questions/static/js/questions.js
@@ -1,9 +1,5 @@
 $(function () {
-  $(".question .panel-body").click(function () {
-    var question_id = $(this).closest(".question").attr("question-id");
-    location.href = "/questions/" + question_id;
-  });
-
+  
   $(".accept").click(function () {
     var span = $(this);
     var question = $(".question").attr("question-id");
